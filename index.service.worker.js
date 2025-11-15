314a300a395156ab3c85c38104f7715183f09d24
Ninjago987 <133258800+ninjago9876@users.noreply.github.com>
2025-11-16 00:14:09 +0100

    Commit 6

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1763247611|13252448525';
+const CACHE_VERSION = '1763248442|14082854379';
 /** @type {string} */
 const CACHE_PREFIX = 'BigLaserCleanupW-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
